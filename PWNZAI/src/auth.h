899778e1e813fac1dc5e184e130b6b1d5b1d2b52
antufrievroman510-del <antufrievroman510@gmail.com>
2026-06-07 21:21:27 +0500

    Delete PWNZAI directory

--- PWNZAI/src/auth.h
+++
@@ -1,6 +0,0 @@
-﻿#pragma once
-#include <string>
-
-std::string GetHWID();
-// ������ ������� ������������� ���������� ���� �������� �� ������
-std::string SendAuthRequest(const std::string& username, const std::string& password, bool is_register, std::string& out_expiry);
